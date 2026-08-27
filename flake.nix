3127c4412f5f76ea279600852b7a4016d7f40c5b
Neo <61031352+glassesneo@users.noreply.github.com>
2026-08-27 11:01:42 +0900

    Nix開発環境を追加 (#4)

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    systems.url = "github:nix-systems/default";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {
+    self,
+    systems,
+    nixpkgs,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} (top: {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+      systems = import systems;
+      perSystem = {pkgs, ...}: {
+        devShells = {
+          default = pkgs.mkShellNoCC {
+            packages = [
+              pkgs.nodejs_22
+              pkgs.typescript-language-server
+            ];
+          };
+        };
+
+        treefmt = {
+          projectRootFile = "flake.nix";
+          programs = {
+            alejandra.enable = true;
+          };
+        };
+      };
+    });
+}
